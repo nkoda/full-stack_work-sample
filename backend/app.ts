78c000e1f9ff455a9183efac6a750827ccb8d816
Nikko <dumriquenikko@gmail.com>
2023-06-22 15:58:46 -0700

    added dev dependencies for development

--- backend/app.ts
+++ backend/app.ts
@@ -1,3 +1,23 @@
-import fs from 'fs';
+import express, { Express, Request, Response, NextFunction } from "express";
 
-fs;
+const app = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => { 
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, DELETE, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+//handle incoming json data
+app.use(express.json()) 
+
+//API pubic routes
+// app.use('/api', publicRoutes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send('<h1> 404: Page not found!!!</h1>')
+})
+
+app.listen(8080);
